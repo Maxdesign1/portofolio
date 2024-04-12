cf9229039e873016be36a4106d2cea6ef864b86b
Maxdesign1 <166862195+Maxdesign1@users.noreply.github.com>
2024-04-12 19:44:45 +0100

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function more() {
+    var fola = document.getElementById('sidecontents');
+    if (fola.style.display == 'block') {
+        fola.style.display = 'none';
+    } else {
+        fola.style.display = 'block';
+    }
+
+}
